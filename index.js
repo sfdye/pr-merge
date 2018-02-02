c2ce2ed3301da756dd1071c644f8a28a347873c1
Wan Liuyang <tsfdye@gmail.com>
2018-02-02 13:44:09 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = (robot) => {
+  robot.on('pull_request.closed', async context => {
+    if (context.payload.pull_request.merged) {
+      const branch = context.payload.pull_request.head.label
+      const params = context.issue({
+        body: `Congratulations on the merge! Don't forget to delete the branch \`${branch}\`.`
+      })
+
+      try {
+        context.github.issues.createComment(params)
+      }
+      catch (err) {
+        if (err.code !== 404) {
+          throw err
+        }
+      }
+    }
+  })
+}
