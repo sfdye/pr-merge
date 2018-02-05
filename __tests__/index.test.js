4271ec4a5e68758dab2e95c94245597819c31133
John <john@stashaway.com>
2018-02-05 18:16:51 +0800

    fix(test) Fix unit tests and code style lint
    - Added unit tests
    - Added .eslintrc with airbnb legacy

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,69 @@
-// You can import your modules
-// const index = require('../index')
+const { createRobot } = require('probot')
+const sinon = require('sinon')
+const app = require('../index')
+const payload = require('./fixtures/payload')
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+const initMock = (github) => {
+  const robot = createRobot()
+  robot.auth = sinon.stub().resolves(github)
+  app(robot)
+
+  return robot
+}
+
+const getGithubMock = () => ({
+  issues: {
+    createComment: sinon.stub()
+  }
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
+class NotFoundError extends Error {
+  constructor (code) {
+    super()
+    this.code = code
+  }
+}
+
+describe('app plugin test', () => {
+  describe('should resolve', () => {
+    let robot
+    let githubMock = getGithubMock()
+
+    beforeEach(() => {
+      robot = initMock(githubMock)
+      githubMock.issues.createComment.resolves()
+    })
+
+    it('should performs an action', async () => {
+      const expectedParams = {
+        number: 21,
+        owner: 'owner',
+        repo: 'testing-things',
+        body: 'Congratulations on the merge :tada: Don\'t forget to delete the branch `This is a label`.'
+      }
+
+      await robot.receive(payload)
+
+      expect(githubMock.issues.createComment.calledOnce).toEqual(true)
+      expect(githubMock.issues.createComment.args[0][0]).toEqual(expectedParams)
+    })
+  })
+
+  describe('should reject', () => {
+    let robot
+    let githubMock = getGithubMock()
+
+    beforeEach(() => {
+      githubMock.issues.createComment.rejects(new NotFoundError())
+      robot = initMock(githubMock)
+    })
+
+    it('should throw exception', async () => {
+      try {
+        await robot.receive(payload)
+      } catch (err) {
+        expect(err instanceof NotFoundError).toEqual(true)
+      }
+    })
+  })
+})
